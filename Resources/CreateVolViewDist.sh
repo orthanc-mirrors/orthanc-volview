eeb91480c7c2e46ab299164552840d0a55ff17f6
Sebastien Jodogne <s.jodogne@gmail.com>
2026-01-02 14:56:11 +0100

    updated year to 2026

--- Resources/CreateVolViewDist.sh
+++ Resources/CreateVolViewDist.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# SPDX-FileCopyrightText: 2023-2025 Sebastien Jodogne, ICTEAM UCLouvain, Belgium
+# SPDX-FileCopyrightText: 2023-2026 Sebastien Jodogne, ICTEAM UCLouvain, Belgium
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # Kitware's VolView plugin for Orthanc
-# Copyright (C) 2023-2025 Sebastien Jodogne, ICTEAM UCLouvain, Belgium
+# Copyright (C) 2023-2026 Sebastien Jodogne, ICTEAM UCLouvain, Belgium
 #
 # This program is free software: you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
